f6e635ec703689c95990d7a12c6fcb3a39461b60
syle3210 <syle3210@gmail.com>
2026-08-20 12:43:53 -0600

    server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.urlencoded({ limit: '100mb', extended: true }));
 const NIM_API_BASE = process.env.NIM_API_BASE || 'https://integrate.api.nvidia.com/v1';
 const NIM_API_KEY = process.env.NIM_API_KEY;
 
-const SHOW_REASONING = process.env.SHOW_REASONING === 'true' || true;
-const ENABLE_THINKING_MODE = process.env.ENABLE_THINKING_MODE === 'true' || true;
+const SHOW_REASONING = process.env.SHOW_REASONING === 'false' || false;
+const ENABLE_THINKING_MODE = process.env.ENABLE_THINKING_MODE === 'false' || false;
 
 const MODEL_MAPPING = {
   'glm-pro': 'z-ai/glm-5.2',
